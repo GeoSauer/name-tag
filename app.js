88a72119755035e0c3ccec14f939dd87b61d5305
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-09-07 15:47:36 -0700

    app.js progress, event listener issues

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // import functions and grab DOM elements
+const nameInput = document.getElementById('name-input');
+const changeButton = document.getElementById('button');
+const nameDisplay = document.getElementById('name-display');
 
 // let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+changeButton.addEventListener('click', ()) => {
+        const name = nameInput.value;
+        console.log(nameInput);
+        console.log(clicking);
+    };
+
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
